ac687e25c64cb7898ebe860f6aefad718725f2b1
AngelT52 <62609637+AngelT52@users.noreply.github.com>
2022-02-16 16:46:58 +0100

    Ajout onglet google map et intégration google map
    https://developers.google.com/maps/documentation/javascript/overview#maps_map_simple-html

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader'
 
 @Component({
   selector: 'app-map',
@@ -10,6 +11,16 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: "add your own api key bro"
+    });
+    
+    loader.load().then(() => {
+       new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+    });
   }
 
 }
