9b3cd0200991d4af97813e5a1da07d16b083c294
AngelT52 <62609637+AngelT52@users.noreply.github.com>
2022-02-16 18:19:47 +0100

    customisation map et marker sur position actuelle
    verifier sur serveur

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,16 +11,218 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(!navigator.geolocation) {
+      console.log('L\'accès a la localisation n\'est pas activée ou n\'est pas possible')
+    }
+    navigator.geolocation.getCurrentPosition((position) =>  {
+      console.log(`lat: ${position.coords.latitude}, long: ${position.coords.longitude}`)
+      loader.load().then(() => {
+        const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: { lat: position.coords.latitude, lng: position.coords.longitude },
+        zoom: 18,
+        maxZoom: 19,
+        minZoom: 16,
+        disableDefaultUI: true,
+        styles: [
+    {
+        "featureType": "all",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "color": "#5b6571"
+            },
+            {
+                "lightness": "35"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#f3f4f4"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "weight": 0.9
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#83cead"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#fee379"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#7fc8ed"
+            }
+        ]
+        
+    }
+    
+]
+      });
+      const myLatLng = { lat: position.coords.latitude, lng: position.coords.longitude };
+      new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: "oh zebi ca marche",
+      });
+      
+    });
+
+      
+    })
     let loader = new Loader({
-      apiKey: "add your own api key bro"
+      apiKey: "add your own"
     });
     
-    loader.load().then(() => {
-       new google.maps.Map(document.getElementById("map") as HTMLElement, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-      });
-    });
   }
 
 }
